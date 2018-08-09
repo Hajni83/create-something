7eac175e13102122b4d445befe5c16a9d6edf9df
József Tar <szurilo@gmail.com>
2018-08-09 22:12:14 +0200

    continue

---
+++ script.js
@@ -0,0 +1,20 @@
+$(function() {
+  function init() {
+    canvas = document.getElementsByTagName("canvas")[0];
+    context = canvas.getContext("2d");
+    context.translate(400, 400);
+
+    var maxCircle = 75;
+    var radius = 200;
+    var size = 10;
+
+    for (var i = 1; i <= maxCircle; i++) {
+      context.fillStyle = 'hsl(' + 360 * Math.random() + ',100%,50%)';
+      context.beginPath();
+      context.arc(0, radius, size,0,2 * Math.PI, false);
+      context.rotate((2 * Math.PI) / maxCircle);
+      context.fill();
+    }
+  }
+  window.addEventListener('load',init,false);
+}());
