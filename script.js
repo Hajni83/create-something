edba5c9a4900ce92217ba96bdb749a5d48417edf
Hajni83 <maro.hajnalka@gmail.com>
2018-08-09 22:29:05 +0200

    fine

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-$(function() {
+$(function () {
   function init() {
     canvas = document.getElementsByTagName("canvas")[0];
     context = canvas.getContext("2d");
     context.translate(400, 400);
 
-    var maxCircle = 75;
-    var radius = 200;
+    var maxCircle = 55;
+    var radius = 300;
     var size = 10;
 
     for (var i = 1; i <= maxCircle; i++) {
       context.fillStyle = 'hsl(' + 360 * Math.random() + ',100%,50%)';
       context.beginPath();
-      context.arc(0, radius, size,0,2 * Math.PI, false);
+      context.arc(0, radius, size, 0, 2 * Math.PI, false);
       context.rotate((2 * Math.PI) / maxCircle);
       context.fill();
     }
   }
-  window.addEventListener('load',init,false);
-}());
+  init();
+});
